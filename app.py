e0200b967af5a6c861d36a8fafe0d603abd41a70
hvonkent <hvonkent@gmail.com>
2026-05-19 16:13:20 -0500

    commit

--- app.py
+++ app.py
@@ -7,19 +7,16 @@ print(cwd)
 
 page_wd = os.path.join(cwd, 'pages')
 
-# Define the pages
-page_name = os.path.join(page_wd, "main_page.py")
+page_name = os.path.join(cwd, "main_page.py")
 main_page = st.Page(page_name, title="Main Page", icon="🎈")
 
-page_name = os.path.join(page_wd, "main_page.py")
+page_name = os.path.join(cwd, "main_page.py")
 page_2 = st.Page(page_name, title="Page 2", icon="❄️")
 
-page_name = os.path.join(page_wd, "main_page.py")
+page_name = os.path.join(cwd, "main_page.py")
 page_3 = st.Page(page_name, title="Page 3", icon="🎉")
 
-# Set up navigation
 pg = st.navigation([main_page, page_2, page_3])
-
 
 st.write("table:")
 st.write(pd.DataFrame({
---
+++ main_page.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+# Main page content
+st.markdown("# Main page 🎈")
+st.sidebar.markdown("# Main page 🎈")
+
+st.write('TBD')
