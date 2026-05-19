41cc4d8440045a094f548424cf1c918f6701c529
hvonkent <hvonkent@gmail.com>
2026-05-19 16:01:11 -0500

    commit

--- app.py
+++ app.py
@@ -5,18 +5,23 @@ import os
 cwd = os.getcwd()
 print(cwd)
 
-"""
+page_wd = os.path.join(cwd, 'pages')
+
 # Define the pages
-main_page = st.Page("main_page.py", title="Main Page", icon="🎈")
-page_2 = st.Page("page_2.py", title="Page 2", icon="❄️")
-page_3 = st.Page("page_3.py", title="Page 3", icon="🎉")
+page_name = os.path.join(page_wd, "main_page.py")
+main_page = st.Page(page_name, title="Main Page", icon="🎈")
+
+page_name = os.path.join(page_wd, "main_page.py")
+page_2 = st.Page(page_name, title="Page 2", icon="❄️")
+
+page_name = os.path.join(page_wd, "main_page.py")
+page_3 = st.Page(page_name, title="Page 3", icon="🎉")
 
 # Set up navigation
 pg = st.navigation([main_page, page_2, page_3])
 
-"""
 
-st.write("Here's our first attempt at using data to create a table:")
+st.write("table:")
 st.write(pd.DataFrame({
     'first column': [1, 2, 3, 4],
     'second column': [10, 20, 30, 40]
