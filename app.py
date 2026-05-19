347fa3f6a00d5802883a3f89663139ed658ab684
hvonkent <hvonkent@gmail.com>
2026-05-19 15:06:38 -0500

    commit

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit as st
+import pandas as pd
 
+"""
 # Define the pages
 main_page = st.Page("main_page.py", title="Main Page", icon="🎈")
 page_2 = st.Page("page_2.py", title="Page 2", icon="❄️")
@@ -7,6 +9,14 @@ page_3 = st.Page("page_3.py", title="Page 3", icon="🎉")
 
 # Set up navigation
 pg = st.navigation([main_page, page_2, page_3])
+
+"""
+
+st.write("Here's our first attempt at using data to create a table:")
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+}))
 
 # Run the selected page
 pg.run()
